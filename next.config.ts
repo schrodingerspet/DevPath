2fcbe2c36c5165a06c887bd51f354e24b5b86370
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-12-14 23:40:22 +0530

    chore: remove log files and update gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    buildActivity: false,
+    appIsrStatus: false,
+  },
   reactCompiler: true,
 };
 
